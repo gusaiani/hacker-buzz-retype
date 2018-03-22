403b0bdc0a8e27594c79fdd47eb2439f9152d126
Gustavo Saiani <gustavo@poe.ma>
2018-03-22 13:34:04 -0300

    Start Story

--- js/containers/ask/navigationConfiguration.js
+++ js/containers/ask/navigationConfiguration.js
@@ -1,8 +1,19 @@
+import { StackNavigator } from 'react-navigation';
 import AskStoriesContainer from './AskStoriesContainer';
+import Story from '../story/Story';
+
+const routeConfiguration = {
+  AskStoriesContainer: { screen: AskStoriesContainer },
+  AskStory: { screen: Story }
+};
 
 const routeConfiguration = {
   AskStoriesContainer: { screen: AskStoriesContainer }
 }
+
+export const NavigatorTabAsk = StackNavigator(
+  routeConfiguration,
+)
 
 export const NavigatorTabAsk = StackNavigator(
   routeConfiguration,
---
+++ js/containers/story/Story.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { TouchableOpacity, View } from 'react-native';
+import { darkTheme } from '../../styles';
+import CommentList from './CommentList';
+
+export class Story extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      refreshing: false,
+      start: 0,
+      end: 4,
+      isSharing: false
+    };
+  }
+
+  static navigationOptions = ({ navigation }) => {
+    const { descendants } = navigation.state.params.story;
+    return {
+      headerLeft: Story.backButton(navigation),
+      headerRight: Story.shareButton(navigation),
+      title: descendants !== undefined
+        ? descendants != 1
+          ? `${descendants} Comments`
+          : `${descendants} Comment`
+        : 'Job',
+      headerTitleStyle: { color: darkTheme.headerTitle },
+      headerStyle: {
+        backgroundColor: darkTheme.headerBackground,
+        borderBottomColor: darkTheme.tabBarOutline,
+        borderBottomWidth: 0.5
+      },
+      headerTinColor: darkTheme.headerBackButton
+    };
+  };
+
+  render() {
+    const { navigation, isFetching, hasErrored, commentsByHash } = this.props;
+    const { story } = navigation.state.params;
+
+    return (
+      <View style={{ flex: 1 }}>
+        <CommentList
+        />
+      </View>
+    )
+  }
+}

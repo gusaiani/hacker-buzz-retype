672e42cf47993f80c1545890451433253e8be952
Gustavo Saiani <gustavo@poe.ma>
2018-03-22 13:10:06 -0300

    Bring StoryListContainer up-to-date

--- js/containers/list/StoryListContainer.js
+++ js/containers/list/StoryListContainer.js
@@ -5,9 +5,11 @@ import {
   fetchMoreStories,
   refreshStories
 } from '../../actions/StoryActions';
+import { favoriteStory, unfavoriteStory } from '../../actions/FavoriteActions';
 import { StoryListErrorMessage } from './StoryListErrorMessage';
 
 import StoryList from './StoryList';
+const storyLimit = 20;
 
 export class StoryListContainer extends Component {
   constructor(props) {
@@ -48,6 +50,44 @@ export class StoryListContainer extends Component {
     }
   };
 
+  handleLoadMore = () => {
+    const { isLoading, stories, ids } = this.props;
+    if (!isLoading && stories.length !== ids.length) {
+      this.setState(
+        {
+          start: this.state.start + storyLimit,
+          end: this.state.end + storyLimit
+        },
+        () => {
+          this.loadMoreStories();
+        }
+      );
+    }
+  };
+
+  handleRefresh = () => {
+    this.refreshStories();
+  };
+
+  refreshStories = () => {
+    const { category, ids } = this.props;
+    this.props.refreshStories(category, ids);
+  };
+
+  loadMoreStories = () => {
+    const { ids } = this.props;
+    const { start, end } = this.state;
+    this.getItems(ids, start, end);
+  };
+
+  handleLeftActionComplete = item => {
+    const { byId } = this.props;
+    const isFavorited = byId.filter(id => id === item.id).length > 0;
+    isFavorited
+      ? this.props.unfavoriteStory(item.id)
+      : this.props.favoriteStory(item);
+  };
+
   render() {
     const { stories, navigation, route, refreshing, hasErrored } = this.props;
 
@@ -59,11 +99,41 @@ export class StoryListContainer extends Component {
         stories={stories}
         refreshing={refreshing}
         navigation={navigation}
-        handleLoadMore={this.handleRefresh}
+        handleLoadMore={this.handleLoadMore}
+        handleRefresh={this.handleRefresh}
+        route={route}
+        handleLeftActionComplete={this.handleLeftActionComplete}
       />
     );
   }
 }
+
+const getAppropriateReducer = (state, category) => {
+  switch (category) {
+    case 'topstories':
+      return state.top;
+    case 'beststories':
+      return state.best;
+    case 'askstories':
+      return state.ask;
+    case 'showstories':
+      return state.show;
+    case 'jobstories':
+      return state.jobs;
+    default:
+      return state.new;
+  }
+};
+
+const mapStateToProps = (state, ownProps) => {
+  const { category } = ownProps;
+  const reducer = getAppropriateReducer(state, category);
+
+  const { stories, ids, isLoading, refreshing, hasErrored } = reducer;
+  const { byId } = state.favorites;
+
+  return { stories, ids, isLoading, refreshing, byId, hasErrored };
+};
 
 const mapDispatchToProps = dispatch => ({
   fetchStories: type => dispatch(fetchStories(type)),

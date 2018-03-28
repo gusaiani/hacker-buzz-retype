2819ad4651320911b516fb378a856fc91ea64b58
Gustavo Saiani <gustavo@poe.ma>
2018-03-28 11:12:28 -0300

    More around StoryHeader

--- js/containers/story/StoryHeader.js
+++ js/containers/story/StoryHeader.js
@@ -1,33 +1,98 @@
 import React, { Component } from 'react';
 import {
   View,
+  Text,
   TouchableOpacity,
   StyleSheet,
 } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import HTMLView from 'react-native-htmlview';
+import { darkTheme } from '../../styles';
 import { openURL } from '../../network/web';
 import { connect } from 'react-redux';
 
 class StoryHeader extends Component {
-  const {
-    container,
-    headerContainer,
-    textContainer,
-  } = styles;
-
   render() {
+    const { story } = this.props;
+    const {
+      container,
+      headerContainer,
+      textContainer,
+      title,
+    } = styles;
+
     return (
       <View style={container}>
         <View style={headerContainer}>
           <View style={textContainer}>
             <TouchableOpacity
               activeOpacity={0.6}
-      onPress={_ => openURL}
+              onPress={_ => openURL(story.url)}
+            >
+              <Text style={title}>
+                {story.title}
+              </Text>
+            </TouchableOpacity>
+            {story.text !== undefined &&
+              <View
+                style={{ marginLeft: 10, marginRight: 10, marginBottom: 10 }}
+              >
+                <HTMLView
+                  value={`<blockquote>${story.text}</blockquote>`}
+                  stylesheet={HTMLstyles}
+                />
+              </View>}
+            <View
+              style={{
+                flex: 1,
+                flexDirection: 'row',
+                justifyContent: 'space-between'
+              }}
+            >
+              <View style={{ flexDirection: 'column' }}>
+                <Text style={author}>
+                  {story.by}
+                </Text>
+                <View style={commentsContainer}>
+                  <Icon
+                </View>
+              </View>
+            </View>
           </View>
         </View>
       </View>
     )
   }
 }
+
+const HTMLstyles = StyleSheet.create({
+  blockquote: {
+    color: darkTheme.commentText
+  },
+  a: {
+    fontWeight: '300',
+    color: darkTheme.commentURL
+  },
+  p: {
+    color: darkTheme.commentText
+  },
+  i: {
+    fontStyle: 'italic',
+    color: darkTheme.commentText
+  },
+  b: {
+    color: darkTheme.commentText,
+    fontWeight: '500'
+  },
+  code: {
+    fontFamily: 'Menlo',
+    color: darkTheme.commentText
+  },
+  pre: {
+    fontFamily: 'Menlo',
+    color: darkTheme.commentText
+  }
+});
 
 const styles = StyleSheet.create({
   container: {
@@ -45,9 +110,29 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'space-between'
   },
+  commentsContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    marginLeft: 10,
+    marginBottom: 5
+  },
+  title: {
+    color: darkTheme.storyTitle,
+    fontSize: 30,
+    marginLeft: 10,
+    marginTop: 10,
+    marginBottom: 5,
+    marginRight: 10
+  },
+  author: {
+    color: darkTheme.storyAuthor,
+    fontSize: 20,
+    marginBottom: 5,
+    paddingLeft: 10
+  },
 });
 
-const mapStateToProps = (state, headerProps) {
+const mapStateToProps = (state, headerProps) => {
   const { byId } = state.favorites;
   const { story } = headerProps;
 
--- js/network/web.js
+++ js/network/web.js
@@ -1,6 +1,8 @@
 import SafariView from 'react-native-safari-view';
-import { Platform } from 'react-native';
+import { Platform, Linking } from 'react-native';
 import { DropDownManager } from '../utils/DropDownManager';
+
+const typeform = 'https://rafique3.typeform.com/to/rquygr';
 
 export const openURL = url => {
   Platform.OS === 'ios' ? openSafariView(url) : openAndroidBrowser(url);
@@ -20,4 +22,26 @@ const openSafariView = url => {
         'Oops, could not open Safari at this time.'
       )
     );
+};
+
+const openAndroidBrowser = url => {
+  Linking.canOpenURL(url)
+    .then(supported => {
+      if (!supported) {
+        DropDownManager.getDropDown().alertWithType(
+          'error',
+          'Error',
+          'Can\'t handle url: ' + url
+        );
+      } else {
+        return Linking.openURL(url);
+      }
+    })
+    .catch(err =>
+      DropDownManager.getDropDown().alertWithType('error', 'Error', err)
+    );
+};
+
+export const openTypeform = () => {
+  openURL(typeform);
 };

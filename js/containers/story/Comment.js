0b721b9b0d5822f336c82ea118a8ca2a2028d93c
Gustavo Saiani <gustavo@poe.ma>
2018-04-05 10:57:56 -0300

    More around CommentList

---
+++ js/containers/story/Comment.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from 'react';
+import StoryCommentPlaceholder from '../../components/StoryCommentPlaceholder';
+
+export default class Comment extends PureComponent {
+  render() {
+    return (
+      <StoryCommentPlaceholder
+    )
+  }
+}
--- js/containers/story/CommentList.js
+++ js/containers/story/CommentList.js
@@ -1,14 +1,34 @@
 import React, { PureComponent } from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, View, ActivityIndicator } from 'react-native';
 import { darkTheme } from '../../styles';
 import StoryHeader from './StoryHeader';
+import Comment from './Comment';
 import { EmptyStory } from './EmptyStory';
+import { StoryListErrorMessage } from '../list/StoryListErrorMessage';
 
 export default class CommentList extends PureComponent {
+  state = {
+    refreshing: false
+  };
+
+  _keyExtractor = item => (item.id ? item.id : item);
+
   renderHeader = () => {
     const { story } = this.props.navigation.state.params;
     return <StoryHeader story={story} />;
   };
+
+  handleRefresh = () => {
+    this.props.handleRefresh();
+  };
+
+  renderItem = ({ item }) => {
+    return (
+      <View>
+        <Comment comment={item} />
+      </View>
+    )
+  }
 
   renderFooter = () => {
     if (this.props.navigation.state.params.story.category === 'jobstories') {
@@ -18,10 +38,31 @@ export default class CommentList extends PureComponent {
     if (this.props.kids === undefined) {
       return <EmptyStory />;
     }
-  }
+
+    if (!this.props.isFetching) return null;
+
+    return (
+      <View style={{ paddingVertical: 20 }}>
+        <ActivityIndicator animating size="large" />
+      </View>
+    );
+  };
+
+  handleOnEndReached = () => {
+    this.props.handleLoadMore();
+  };
 
   render() {
-    const { comments, kids } = this.props;
+    const { comments, kids, hasErrored } = this.props;
+
+    if (hasErrored) {
+      return (
+        <View style={{ flex: 1, backgroundColor: darkTheme.storyBackground }}>
+          <StoryListErrorMessage handleTryAgain={this.handleRefresh} />
+        </View>
+      );
+    }
+
     return (
       <FlatList
         style={{ backgroundColor: darkTheme.tabInactiveBackground }}
@@ -34,8 +75,13 @@ export default class CommentList extends PureComponent {
         renderItem={this.renderItem}
         ListHeaderComponent={this.renderHeader}
         ListFooterComponent={this.renderFooter}
-
+        refreshing={this.state.refreshing}
+        onRefresh={this.handleRefresh}
+        onEndReached={this.handleOnEndReached}
+        onEndReachedThreshold={5}
+        removeClippedSubviews={false}
+        indicatorStyle={'white'}
       />
-    )
+    );
   }
 }

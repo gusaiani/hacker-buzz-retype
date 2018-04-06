09888d1b8f76f6a31c8e76805a69bf57d441e340
Gustavo Saiani <gustavo@poe.ma>
2018-04-06 12:04:14 -0300

    Bring Comment up-to-date

--- js/containers/story/Comment.js
+++ js/containers/story/Comment.js
@@ -9,6 +9,9 @@ import {
 } from 'react-native';
 import { darkTheme } from '../../styles';
 import StoryCommentPlaceholder from '../../components/StoryCommentPlaceholder';
+import { convertTimestamp } from '../../utils';
+import HTMLView from 'react-native-htmlview';
+import Collapsible from 'react-native-collapsible';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Shine from '../../utils/animations/shine';
 
@@ -20,6 +23,14 @@ export default class Comment extends PureComponent {
       spinValue: new Animated.Value(0)
     };
     this._animated = new Animated.Value(0.25);
+  }
+
+  componentDidMount() {
+    Animated.timing(this._animated, {
+      toValue: 1,
+      duration: 750,
+      useNativeDriver: true
+    }).start();
   }
 
   toggleExpanded = () => {
@@ -48,7 +59,12 @@ export default class Comment extends PureComponent {
   };
 
   render() {
-    const { text, by } = this.props.comment;
+    const { deleted, text, kids, by, time } = this.props.comment;
+    const { collapsed } = this.state;
+    const spin = this.state.spinValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '180deg']
+    });
     const {
       container,
       textContainer,
@@ -60,6 +76,12 @@ export default class Comment extends PureComponent {
       divider,
       placeholder
     } = styles;
+
+    if (deleted) {
+      return null;
+    }
+
+    const hasReplies = kids && kids.length;
 
     const containerStyles = [container, { opacity: this._animated }];
 
@@ -89,13 +111,66 @@ export default class Comment extends PureComponent {
                 {by}
               </Text>
               <Text style={divider}>●</Text>
+              <Text style={timeAgo}>
+                {convertTimestamp(time)}
+              </Text>
             </View>
           </TouchableWithoutFeedback>
+          <Collapsible collapsedHeight={0} collapsed={collapsed} duration={600}>
+            <Animated.View style={textContainer}>
+              <HTMLView
+                value={`<blockquote>${text}</blockquote>`}
+                stylesheet={HTMLstyles}
+              />
+            </Animated.View>
+          </Collapsible>
+          {hasReplies &&
+            <View style={{ marginLeft: 10, marginTop: 5 }}>
+              {kids.map(
+                kid =>
+                  kid.id &&
+                  <Collapsible key={kid.id} collapsed={collapsed}>
+                    <Animated.View key={kid.id} style={subCommentContainer}>
+                      <Animated.View style={commentLine} />
+                      <Comment key={kid.id} comment={kid} />
+                    </Animated.View>
+                  </Collapsible>
+              )}
+            </View>}
         </Animated.View>
       </StoryCommentPlaceholder>
-    )
+    );
   }
-};
+}
+
+const HTMLstyles = StyleSheet.create({
+  blockquote: {
+    color: darkTheme.commentText
+  },
+  a: {
+    fontWeight: '300',
+    color: darkTheme.commentURL
+  },
+  p: {
+    color: darkTheme.commentText
+  },
+  i: {
+    fontStyle: 'italic',
+    color: darkTheme.commentText
+  },
+  b: {
+    color: darkTheme.commentText,
+    fontWeight: '500'
+  },
+  code: {
+    fontFamily: 'Menlo',
+    color: darkTheme.commentText
+  },
+  pre: {
+    fontFamily: 'Menlo',
+    color: darkTheme.commentText
+  }
+});
 
 const styles = StyleSheet.create({
   container: {
@@ -108,6 +183,25 @@ const styles = StyleSheet.create({
     marginTop: 5,
     marginBottom: 5
   },
+  textContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    marginLeft: 10,
+    marginBottom: 5,
+    marginRight: 10
+  },
+  subCommentContainer: {
+    marginTop: -5,
+    justifyContent: 'space-between',
+    flexDirection: 'row'
+  },
+  commentLine: {
+    backgroundColor: '#333333',
+    width: 1,
+    marginTop: 10,
+    marginBottom: 5
+  },
   divider: {
     color: darkTheme.storyDivider,
     fontSize: 5,
@@ -118,6 +212,11 @@ const styles = StyleSheet.create({
     fontSize: 15,
     paddingTop: 5,
     fontWeight: '500'
+  },
+  timeAgo: {
+    color: darkTheme.storyTimeAgo,
+    fontSize: 15,
+    paddingTop: 5
   },
   placeholder: {
     flex: 1,

1b4238388b1f3c8a9c8885ed5df9d8a888ba90ab
Gustavo Saiani <gustavo@poe.ma>
2018-04-17 09:52:07 -0300

    Bring ShareStory up-to-date

--- js/components/ShareStory.js
+++ js/components/ShareStory.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { Clipboard } from 'react-native';
 import Share, { ShareSheet } from 'react-native-share';
+import { darkTheme } from '../styles';
 import ShareItem from './ShareItem';
+import { DropDownManager } from '../utils/DropDownManager';
 
 export default class ShareStory extends Component {
   onCancel = () => {
@@ -14,12 +17,33 @@ export default class ShareStory extends Component {
     }, 300);
   };
 
+  shareOpenMore = shareOptions => {
+    this.onCancel();
+    setTimeout(() => {
+      Share.open(shareOptions).catch(() => {});
+    }, 300);
+  };
+
+  shareCopyLink = shareOptions => {
+    this.onCancel();
+    setTimeout(() => {
+      if (typeof shareOptions['url'] !== undefined) {
+        Clipboard.setString(shareOptions['url']);
+        DropDownManager.getDropDown().alertWithType(
+          'custom',
+          'Copied Link URL',
+          ''
+        );
+      }
+    }, 300);
+  };
+
   render() {
     const { story } = this.props;
     let shareOptions = {
       title: story.title,
       subject: `Hacker News: ${story.title}`,
-      message: `"${story.ttile}" via HackerNewsReader for iOS & Android`,
+      message: `"${story.title}" via HackerNewsReader for iOS & Android`,
       url: `https://news.ycombinator.com/item?id=${story.id}`
     };
 
@@ -31,21 +55,22 @@ export default class ShareStory extends Component {
           onPress={_ => this.shareSocialMedia(shareOptions, 'twitter')}
           name={'Twitter'}
           icon={'twitter'}
-          color={darkTeme.twitter}
+          color={darkTheme.twitter}
         />
         <ShareItem
           onPress={_ => this.shareSocialMedia(shareOptions, 'facebook')}
           name={'Facebook'}
           icon={'facebook'}
+          color={darkTheme.facebook}
         />
         <ShareItem
           onPress={_ => this.shareSocialMedia(shareOptions, 'whatsapp')}
-          name={'Whatsapp'}
+          name={'WhatsApp'}
           icon={'whatsapp'}
           color={darkTheme.whatsapp}
         />
         <ShareItem
-          onPress={_ => this.shareSocialMedia(shareOptions, 'ggogleplus')}
+          onPress={_ => this.shareSocialMedia(shareOptions, 'googleplus')}
           name={'Google +'}
           icon={'google-plus'}
           color={darkTheme.googleplus}
@@ -60,11 +85,15 @@ export default class ShareStory extends Component {
           onPress={_ => this.shareCopyLink(shareOptions)}
           name={'Copy Link'}
           icon={'link'}
-          color={darkTheme.copyLink}
+          color={darkTheme.copylink}
         />
         <ShareItem
-          onPress={_ => this.}
+          onPress={_ => this.shareOpenMore(shareOptions)}
+          name={'More'}
+          icon={'ellipsis-h'}
+          color={darkTheme.email}
+        />
       </ShareSheet>
-    )
+    );
   }
 }
